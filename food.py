71de097cd9c62508a1dd52e479fb3fba99bb664c
desnica <maxim297000@gmail.com>
2025-03-04 18:40:48 +0300

    lolo

--- food.py
+++ food.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f92af74ccbf5aaac2be4b963d800ea01708b0ab94d03c941fd1eb27ab4bd8210
-size 14911
+oid sha256:e82e2958c52929f117b65c952f97d5c53adc067f47ab702b07f10f11bcc1f1c8
+size 15265
